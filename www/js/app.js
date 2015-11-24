9cfa7adea260c86198cfd7995b65ef1fac5faeca
Pedro Silva <pedrro2@hotmail.com>
2015-11-24 11:23:43 -0200

    @pedrro @josigama - adding google maps to application

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,7 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
@@ -17,3 +10,44 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('map', {
+    url: '/',
+    templateUrl: 'templates/map.html',
+    controller: 'MapCtrl'
+  });
+  $urlRouterProvider.otherwise("/");
+})
+.controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
+ var options = {timeout: 10000, enableHighAccuracy: true};
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+   $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+   google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+    var marker = new google.maps.Marker({
+        map: $scope.map,
+        animation: google.maps.Animation.DROP,
+        position: latLng
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+        content: "Here I am!"
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infoWindow.open($scope.map, marker);
+    });
+
+  });
+
+  }, function(error){
+    console.log("Could not get location");
+  });
+});
