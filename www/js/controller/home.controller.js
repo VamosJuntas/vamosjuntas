548aa21acb60e56a8a215b3a4cc6e54ed2b46454
Tayane Fernandes <tayane.rmf@gmail.com>
2016-01-27 15:04:04 -0200

    29 @tayanefernandes Changing the factory to return a object with title and quantity

--- www/js/controller/home.controller.js
+++ www/js/controller/home.controller.js
@@ -2,5 +2,4 @@ angular.module('vamosJuntas').controller('HomeController',
  ['$scope', 'placeFactory', function($scope, placeFactory) {
  	placeFactory.fetchPlaces();
  	$scope.places = placeFactory.myPlaces;
-	console.log(placeFactory, 'placeFactory');
 }]);
