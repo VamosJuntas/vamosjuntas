bde54ebba3b7b8cd47004eb83e617db484e64e67
josigama <josyvisa@gmail.com>
2016-02-20 17:49:46 -0200

    @josigama @viniciusgama - add variable direct connect to config

--- www/test/functional/conf.js
+++ www/test/functional/conf.js
@@ -7,6 +7,7 @@ exports.config = {
 };
 
 if (process.env.SNAP_CI) {
+  exports.config.directConnect = true;
   exports.config.chromeDriver = '/usr/local/bin/chromedriver';
 } else {
   exports.config.seleniumAddress = 'http://localhost:4444/wd/hub';
