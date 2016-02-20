a5832838f1343a0b5c7b16d251022988e83567e8
josigama <josyvisa@gmail.com>
2016-02-20 11:09:15 -0200

    @josigama @jonathanrz - return seleniumAddress to try to solved snap ci problems and defined chrome

--- www/test/functional/conf.js
+++ www/test/functional/conf.js
@@ -1,5 +1,11 @@
 exports.config = {
+
+  capabilities: {
+  'browserName': 'chrome'
+  },
+
   framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['report.spec.js'],
 };
 
