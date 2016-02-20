fc769d93e38485f3c3efc86915b6b873decc1b78
josigama <josyvisa@gmail.com>
2016-02-20 09:57:23 -0200

    @josigama @pamcdm - configure functional test in snap ci

--- www/test/functional/conf.js
+++ www/test/functional/conf.js
@@ -3,3 +3,7 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['report.spec.js'],
 };
+
+if (process.env.SNAP_CI) {
+  exports.config.chromeDriver = '/usr/local/bin/chromedriver';
+}
