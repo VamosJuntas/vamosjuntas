1ba62746705d6a2db746ee10018685b006a1bc88
josigama <josyvisa@gmail.com>
2016-02-23 20:18:11 -0300

    #41 @lipemorais @tayanefernandes @josigama - creates functional test for the report flow

--- www/js/controller/splash.controller.js
+++ www/js/controller/splash.controller.js
@@ -1,5 +1,5 @@
-angular.module('vamosJuntas').controller('SplashController',['$scope','$state','$location',function($scope, $state, $location) { 
+angular.module('vamosJuntas').controller('SplashController',['$scope','$state','$location',function($scope, $state, $location) {
 	$scope.submit = function() {
-		$location.path('/placeDetails');
+		$location.path('/home');
 	};
 }]);
--- www/test/functional/report.spec.js
+++ www/test/functional/report.spec.js
@@ -32,13 +32,24 @@ describe('Report address form', function() {
     expect(element(by.buttonText('Enviar')).isEnabled()).toBe(false);
   });
 
-  it('should send to confirmation page', function() {
-    browser.get('http://localhost:8100/#/report');
+  it('should go through whole report flow and go back to home', function() {
+    browser.get('http://localhost:8100/');
+
+    element(by.css('#check-terms')).click();
+    element(by.css('button')).click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:8100/#/home');
+
+    element(by.buttonText('Reportar risco')).click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:8100/#/report');
+
     element(by.className('address')).sendKeys('My address');
     element(by.className('date')).sendKeys('01/30/2015');
     element(by.cssContainingText('option', 'Madrugada')).click();
     element(by.cssContainingText('option', 'Roubo')).click();
     element(by.buttonText('Enviar')).click();
     expect(browser.getCurrentUrl()).toEqual('http://localhost:8100/#/confirmation');
+
+    element(by.buttonText('Voltar para a lista')).click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:8100/#/home');
   });
 });
