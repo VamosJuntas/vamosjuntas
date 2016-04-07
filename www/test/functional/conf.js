99d739ec94f935f5fade809e44f3985c7e562cb9
Fernanda Martins <flmmartins@gmail.com>
2016-04-07 19:28:31 -0300

    #00 Fer: Makes all tasks npm based including functional tests

--- www/test/functional/conf.js
+++ www/test/functional/conf.js
@@ -1,15 +1,7 @@
 exports.config = {
   directConnect : true,
-  capabilities: {
-    'browserName': 'chrome'
-  },
   framework: 'jasmine2',
   specs: ['report.spec.js'],
 };
 
-if (process.env.SNAP_CI) {
-  exports.config.directConnect = true;
-  exports.config.chromeDriver = '/usr/local/bin/chromedriver';
-} else {
-  exports.config.seleniumAddress = 'http://localhost:4444/wd/hub';
-}
+seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.52.0.jar'
