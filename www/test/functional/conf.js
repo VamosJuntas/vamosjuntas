6316d87e67a3dc4b9ccadd387a4ddd80d36896d5
Fernanda Martins <flmmartins@gmail.com>
2016-04-08 17:17:15 -0300

    #66 Fer: When on snap forces protractor not to start selenium and use direct testing with browsers

--- www/test/functional/conf.js
+++ www/test/functional/conf.js
@@ -4,6 +4,7 @@ exports.config = {
   specs: ['report.spec.js'],
 };
 if (process.env.SNAP_CI) {
+    exports.config.directConnect = true;
     exports.config.chromeDriver = '/usr/local/bin/chromedriver';
 }
 seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.52.0.jar'
