2bd13b1a9d277d9eba6a7dd8b76d24841894e16a
Fernanda Martins <flmmartins@gmail.com>
2016-04-08 17:09:14 -0300

    #66 Fer: Changes protractor conf for SNAP

--- www/test/functional/conf.js
+++ www/test/functional/conf.js
@@ -3,5 +3,7 @@ exports.config = {
   framework: 'jasmine2',
   specs: ['report.spec.js'],
 };
-
+if (process.env.SNAP_CI) {
+    exports.config.chromeDriver = '/usr/local/bin/chromedriver';
+}
 seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.52.0.jar'
