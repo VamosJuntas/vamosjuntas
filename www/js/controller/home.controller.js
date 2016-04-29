132b3355b8789be810f5b789fcb9cac2d72a54e7
Pedro Silva <pedrro2@hotmail.com>
2016-04-29 13:02:23 -0300

    #35 - @pedrro - updating getTotalOfOccurrences@homeController

--- www/js/controller/home.controller.js
+++ www/js/controller/home.controller.js
@@ -4,10 +4,8 @@ angular.module('vamosJuntas').controller('HomeController', ['$scope', 'placeFact
   });
 
   $scope.getTotalOfOccurrences = function(place) {
-    var numberOfOccurrences = place.occurrences.map(function(occurrence) {
-      return occurrence.numberOfOccurrences;
-    }).reduce(function(a, b) {
-      return a + b;
+    var numberOfOccurrences = place.occurrences.reduce(function(total, occurrence) {
+      return total + occurrence.numberOfOccurrences;
     }, 0);
 
     return numberOfOccurrences;
