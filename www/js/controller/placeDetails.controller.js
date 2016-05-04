0591b1b47f5b15b8fa5735c20f1acc0b732bd1ad
William Ahrons <william.ahrons@gmail.com>
2016-05-04 16:30:02 -0300

    @willmenn removing unused code

--- www/js/controller/placeDetails.controller.js
+++ www/js/controller/placeDetails.controller.js
@@ -2,14 +2,6 @@ angular.module('vamosJuntas').controller('placeDetailsController', ['$scope', 'p
 
   $scope.placeDetails = placeFactory.getPlace();
 
-  $scope.getTotalOfOccurrences = function(place) {
-    var numberOfOccurrences = place.reduce(function(total, occurrence) {
-      return total + numberOfOccurrences;
-    }, 0);
-
-    return numberOfOccurrences;
-  };
-
   $scope.submit = function() {
     $location.path('/report');
   };
