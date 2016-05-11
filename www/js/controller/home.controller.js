5db5e88b64444c9b34ba42f8f6f4a9f7be3996c4
Tayane Fernandes <tayane.rmf@gmail.com>
2016-05-11 09:12:56 -0300

    #32 tayane/lucas: Fixing identation

--- www/js/controller/home.controller.js
+++ www/js/controller/home.controller.js
@@ -1,30 +1,30 @@
 angular.module('vamosJuntas').controller('HomeController',
  ['$scope', 'placeFactory', 'addressFactory',  function($scope, placeFactory, addressFactory) {
-    placeFactory.fetchPlaces().then(function(response) {
-        $scope.places = response.data;
+  placeFactory.fetchPlaces().then(function(response) {
+    $scope.places = response.data;
+  });
+  $scope.search = {};
+  $scope.addresses;
+
+  $scope.getTotalOfOccurrences = function(place) {
+    var numberOfOccurrences = place.occurrences.reduce(function(total, occurrence) {
+      return total + occurrence.numberOfOccurrences;
+    }, 0);
+
+    return numberOfOccurrences;
+  };
+
+  $scope.getSpecificPlace = function(place) {
+    placeFactory.addPlace(place);
+  };
+
+  $scope.searchAddress = function() {
+    addressFactory.getAutoCompleteFromAddress($scope.search.text).then(function(response) {
+      $scope.addresses = response.data.predictions;
     });
-    $scope.search = {};
-    $scope.addresses;
+  };
 
-    $scope.getTotalOfOccurrences = function(place) {
-        var numberOfOccurrences = place.occurrences.reduce(function(total, occurrence) {
-          return total + occurrence.numberOfOccurrences;
-        }, 0);
-
-        return numberOfOccurrences;
-    };
-
-    $scope.getSpecificPlace = function(place) {
-        placeFactory.addPlace(place);
-    };
-
-    $scope.searchAddress = function() {
-      addressFactory.getAutoCompleteFromAddress($scope.search.text).then(function(response) {
-        $scope.addresses = response.data.predictions;
-      });
-    };
-
-    $scope.confirmAddress = function (place) {
-    	$scope.search.text = place.description;
-    }
+  $scope.confirmAddress = function (place) {
+    $scope.search.text = place.description;
+  }
 }]);
