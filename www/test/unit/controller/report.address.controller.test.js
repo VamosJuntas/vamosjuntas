4d4411a6bda8ec9581f18a1d8feb97f5a40b7a21
pedro silva <pedrro2@hotmail.com>
2016-06-23 18:44:02 -0300

    #3 - @pedrro - fixing merge with master

--- www/test/unit/controller/report.address.controller.test.js
+++ www/test/unit/controller/report.address.controller.test.js
@@ -1,33 +1,95 @@
+// describe('ReportAddressController', function() {
+//
+//   beforeEach(function() {
+//     module('vamosJuntas');
+//     inject(function($rootScope, $controller, $injector, $httpBackend, _$location_) {
+//       scope = $rootScope.$new();
+//       $location = _$location_;
+//       $httpBackend.whenGET(/templates.*/).respond('');
+//
+//       spyOn($location, 'path');
+//       createController = function(stateParams) {
+//         var $stateParams;
+//         var defaultStateParams = {
+//           address: ''
+//         };
+//         if (stateParams === undefined) {
+//           $stateParams = defaultStateParams;
+//         } else {
+//           $stateParams = stateParams;
+//         }
+//
+//         $controller('ReportAddressController', {
+//           '$scope': scope,
+//           '$location': $location,
+//           '$stateParams': $stateParams
+//         });
+//       };
+//     });
+//
+//     place = {
+//       "address": "Av. Ipiranga",
+//       "location": {
+//         "latitude": 10,
+//         "longitude": 20
+//       },
+//       "occurrences": [{
+//         "risk": "Roubo",
+//         "count": 2,
+//         "reports": [{
+//           "date": "10/10/2016",
+//           "period": "Manhã"
+//         }, {
+//           "date": "12/10/2016",
+//           "period": "Manhã"
+//         }]
+//       }, {
+//         "risk": "Local Mal Iluminado",
+//         "count": 2,
+//         "reports": [{
+//           "date": "10/10/2016",
+//           "period": "Manhã"
+//         }, {
+//           "date": "12/10/2016",
+//           "period": "Manhã"
+//         }]
+//       }]
+//     };
+//     spyOn(placeFactory, 'getPlace').and.callThrough();
+//     spyOn(placeFactory, 'getPlace').and.returnValues(place);
+//   });
+// });
 describe('ReportAddressController', function() {
-  var scope, placeFactory, $location, place;
+  var scope, state, $location, placeFactory, place;
 
   beforeEach(function() {
     module('vamosJuntas');
-    inject(function($rootScope, $controller, $injector, $httpBackend, _$location_) {
-      scope = $rootScope.$new();
-      $location = _$location_;
-      $httpBackend.whenGET(/templates.*/).respond('');
 
-      spyOn($location, 'path');
-      createController = function(stateParams) {
-        var $stateParams;
-        var defaultStateParams = {
-          address: ''
+    inject(function ($rootScope, $controller, $injector, $httpBackend, _$location_) {
+        scope = $rootScope.$new();
+        $location = _$location_;
+        $httpBackend.whenGET(/templates.*/).respond('');
+
+        spyOn($location, 'path');
+        createController = function(stateParams) {
+          var $stateParams;
+          var defaultStateParams = {
+            address: ''
+          };
+          if(stateParams === undefined) {
+            $stateParams = defaultStateParams;
+          } else {
+            $stateParams = stateParams;
+          }
+
+          $controller('ReportAddressController', {
+            '$scope': scope,
+            '$location': $location,
+            '$stateParams': $stateParams
+          });
         };
-        if (stateParams === undefined) {
-          $stateParams = defaultStateParams;
-        } else {
-          $stateParams = stateParams;
-        }
 
-        $controller('ReportAddressController', {
-          '$scope': scope,
-          '$location': $location,
-          '$stateParams': $stateParams
-        });
-      };
     });
-
     place = {
       "address": "Av. Ipiranga",
       "location": {
@@ -56,8 +118,53 @@ describe('ReportAddressController', function() {
         }]
       }]
     };
+  });
 
-    spyOn(placeFactory, 'getPlace').and.returnValue(place);
+    describe('Saving data', function() {
+      it('should save a new occurrence for existent risk', function() {
+        createController();
+        scope.placeDetails = place;
+        scope.report = {
+          "risk": "Roubo",
+          "date": "10/10/2016",
+          "period": "Manhã"
+        };
+        scope.submit(true);
+        scope.$apply();
+        expect(scope.placeDetails.occurrences[0].count).toEqual(3);
+      });
+
+      it('should save a new occurrence for a non existent risk', function() {
+        createController();
+        scope.placeDetails = place;
+        scope.report = {
+          "risk": "Abuso",
+          "date": "10/10/2016",
+          "period": "Manhã"
+        };
+        scope.submit(true);
+        scope.$apply();
+        expect(scope.placeDetails.occurrences.length).toEqual(3);
+        expect(scope.placeDetails.occurrences[2].count).toEqual(1);
+      });
+    });
+
+  describe('when submit form', function() {
+    describe('and form is valid', function() {
+      it('should redirect to the success page', function() {
+        var controller = createController();
+        scope.submit(true);
+        expect($location.path).toHaveBeenCalledWith('/confirmation');
+      });
+    });
+
+    describe('and form is not valid', function() {
+      it('should not redirect to the success page if address is empty', function() {
+        var controller = createController();
+        scope.submit(false);
+        expect($location.path).not.toHaveBeenCalled();
+      });
+    });
   });
 
   describe('when an address is received', function() {
@@ -69,48 +176,4 @@ describe('ReportAddressController', function() {
       expect(scope.report.address).toEqual(stateParams.address);
     });
   });
-
-  describe('and form is not valid', function() {
-    it('should not redirect to the success page if address is empty', function() {
-      var controller = createController();
-      scope.submit(false);
-      expect($location.path).not.toHaveBeenCalled();
-    });
-  });
-
-  describe('Saving data', function() {
-    it('should save a new occurrence for existent risk', function() {
-      createController();
-      scope.placeDetails = placeFactory.getPlace();
-      scope.report = {
-        "risk": "Roubo",
-        "date": "10/10/2016",
-        "period": "Manhã"
-      };
-      scope.submit(true);
-      scope.$apply();
-      expect(scope.placeDetails.occurrences[0].count).toEqual(3);
-    });
-
-    it('should save a new occurrence for a non existent risk', function() {
-      createController();
-      scope.placeDetails = placeFactory.getPlace();
-      scope.report = {
-        "risk": "Abuso",
-        "date": "10/10/2016",
-        "period": "Manhã"
-      };
-      scope.submit(true);
-      scope.$apply();
-      expect(scope.placeDetails.occurrences.length).toEqual(3);
-      expect(scope.placeDetails.occurrences[2].count).toEqual(1);
-    });
-
-    it('should not redirect to the success page if address is empty', function() {
-      createController();
-      scope.submit(false);
-      expect($location.path).not.toHaveBeenCalled();
-    });
-  });
-
 });
