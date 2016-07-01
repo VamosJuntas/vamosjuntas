63c7f344cbd81cc6218d38f299f51bb86050a767
Juliana Fernandes <littlejuh@live.it>
2016-07-01 14:37:01 -0300

    #60 Juliana/Andriele/Rodrigo Test autocomplete directive

--- www/js/directives/autocomplete.js
+++ www/js/directives/autocomplete.js
@@ -1,7 +1,7 @@
 angular.module('vamosJuntas').directive('googleplace', function() {
     return {
         link: function(scope, element) {
-            scope.gPlace = new google.maps.places.Autocomplete(element[0]);
+            new google.maps.places.Autocomplete(element[0]);
         }
     }
 });
