dd66f585b8cd8375b89184e5d10be6a283a897e1
Tayane Fernandes <tayane.rmf@gmail.com>
2016-07-30 16:31:01 -0300

    #103 Tania/Tayane: Functional test for the report form

--- www/test/functional/report.spec.js
+++ www/test/functional/report.spec.js
@@ -43,6 +43,20 @@ describe('Report address form', function() {
     expect(report.submitButton().isEnabled()).toBe(false);
   });
 
+  it('should clean the form when coming back from confirmation page', function() {
+    browser.get('/#/report');
+    element(by.className('address')).sendKeys('My address');
+
+    var date = new Date(2015, 1, 30, 2, 0);
+    report.fillDate(date);
+    report.fillRisk('Roubo');
+    expect(report.submitButton().isEnabled()).toBe(true);
+    report.submitButtonClick();
+    expect(browser.getCurrentUrl()).toContain('/confirmation');
+    browser.navigate().back();
+    expect(report.getAddress()).toEqual('');
+  });
+
   it('should not send to confirmation page without risk', function() {
     browser.get('/#/report');
 
