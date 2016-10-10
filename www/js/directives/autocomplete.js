0ff0746b2ac42cfab34aabb3c6256e2e7b7a3952
Josi Gama <josyvisa@gmail.com>
2016-10-09 21:04:52 -0300

    @josigama @viniciusgama - #111 fix the autocomplete for the new report

--- www/js/directives/autocomplete.js
+++ www/js/directives/autocomplete.js
@@ -1,7 +1,12 @@
 angular.module('vamosJuntas').directive('googleplace', function() {
     return {
         link: function(scope, element) {
-            new google.maps.places.Autocomplete(element[0]);
+            var autocomplete = new google.maps.places.Autocomplete(element[0]);
+            google.maps.event.addListener(autocomplete, 'place_changed', function() {
+            var place = autocomplete.getPlace();
+            scope.search.text = place.formatted_address;
+            scope.$apply();
+          });
         }
     }
 });
