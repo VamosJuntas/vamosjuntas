d509dbda262ca7665646f2ada8cab91437625a27
pedro silva <pedrro2@hotmail.com>
2017-01-30 11:00:14 -0200

    #113 - @psilva - move constants para app.js

--- www/js/app.js
+++ www/js/app.js
@@ -5,57 +5,59 @@
 // the 2nd parameter is an array of 'requires'
 
 angular.module('vamosJuntas', ['ionic', 'ngCordova'])
+    .constant('ApiEndpoint', {
+        url: 'http://localhost:3001/reports'
+    })
+    .constant('PlacesAPI', {
+        autoCompleteBaseUrl: AppSettings.mapsAPIBaseUrl + '/autocomplete/json?',
+        coordinatesBaseUrl: AppSettings.mapsAPIBaseUrl + '/details/json?',
+        nearbySearchBaseUrl: AppSettings.mapsAPIBaseUrl + '/nearbysearch/json?'
+    })
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('splash', {
+                url: '/',
+                templateUrl: 'templates/splash.html',
+                controller: 'SplashController'
+            })
+            .state('confirmation', {
+                url: '/confirmation',
+                templateUrl: 'templates/confirmation.html'
+            })
+            .state('report', {
+                url: '/report',
+                templateUrl: 'templates/report-address.html',
+                controller: 'ReportAddressController',
+                cache: false,
+                params: {
+                    address: '',
+                    latitude: '',
+                    longitude: ''
+                }
+            })
+            .state('home', {
+                url: '/home',
+                templateUrl: 'templates/home.html',
+                controller: 'HomeController'
+            })
+            .state('placeDetails', {
+                url: '/placeDetails',
+                templateUrl: 'templates/placeDetails.html',
+                controller: 'placeDetailsController'
+            });
 
-.constant('ApiEndpoint', {
-  url: 'http://localhost:3001/reports'
-})
+        $urlRouterProvider.otherwise('/');
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-  .state('splash', {
-    url: '/',
-    templateUrl: 'templates/splash.html',
-    controller: 'SplashController'
-  })
-  .state('confirmation', {
-    url: '/confirmation',
-    templateUrl: 'templates/confirmation.html'
-  })
-  .state('report', {
-    url: '/report',
-    templateUrl: 'templates/report-address.html',
-    controller: 'ReportAddressController',
-    cache: false,
-    params: {
-      address: '',
-      latitude: '',
-      longitude: ''
-    }
-  })
-  .state('home', {
-    url: '/home',
-    templateUrl: 'templates/home.html',
-    controller: 'HomeController'
-  })
-  .state('placeDetails', {
-    url: '/placeDetails',
-    templateUrl: 'templates/placeDetails.html',
-    controller: 'placeDetailsController'
-  });
-
-  $urlRouterProvider.otherwise('/');
-
-});
+    });
