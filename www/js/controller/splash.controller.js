0531aab1ef580ed24ebef1f59f4a7507de4cf924
Josi Gama <josyvisa@gmail.com>
2017-02-09 23:19:28 -0200

    @josigama #97 create new page to terms of use

--- www/js/controller/splash.controller.js
+++ www/js/controller/splash.controller.js
@@ -1,5 +1,16 @@
-angular.module('vamosJuntas').controller('SplashController',['$scope','$state','$location',function($scope, $state, $location) {
+angular.module('vamosJuntas')
+  .controller('SplashController',['$scope', '$state', '$location', '$ionicPopup', function($scope, $state, $location, $ionicPopup) {
     $scope.submit = function() {
         $location.path('/home');
+    };
+    $scope.showAlert = function() {
+      var alertPopup = $ionicPopup.alert({
+        templateUrl: 'templates/terms-of-use.html',
+        title: 'Termos de uso'
+      });
+
+      alertPopup.then(function(res) {
+        console.log('Thank you for not eating my delicious ice cream cone');
+      });
     };
 }]);
