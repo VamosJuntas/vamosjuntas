13ea8e18769ca90b87aff9fb13a2713a46b4d408
Josi Gama <josyvisa@gmail.com>
2017-02-12 22:00:42 -0200

    @josigama - #132 Changing the google maps hey

--- templates/app-settings.js
+++ templates/app-settings.js
@@ -1,10 +1,12 @@
 AppSettings = {
   // @if NODE_ENV == 'DEVELOPMENT'
   mapsAPIBaseUrl: '/maps/api/place',
-  apiBaseUrl: 'http://localhost:3001'
+  apiBaseUrl: 'http://localhost:3001',
+  apiKey: '@@apiKey'
   // @endif
   // @if NODE_ENV == 'PRODUCTION'
   mapsAPIBaseUrl: 'https://maps.googleapis.com/maps/api/place',
-  apiBaseUrl: '@@apiBaseUrl'
+  apiBaseUrl: '@@apiBaseUrl',
+  apiKey: '@@apiKey'
   // @endif
 }
