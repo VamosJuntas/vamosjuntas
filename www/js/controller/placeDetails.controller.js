12c13826026c0836eae38dd219c007fa7e4befdd
Eduardo Braga <ebfjunior@gmail.com>
2017-06-24 18:01:54 -0300

    Adiciona nome da ocorrencia, horario e periodo da ocorrencia na tela de detalhes.

--- www/js/controller/placeDetails.controller.js
+++ www/js/controller/placeDetails.controller.js
@@ -2,6 +2,10 @@ angular.module('vamosJuntas')
     .controller('placeDetailsController', ['$scope', 'placeFactory', '$location', function($scope, placeFactory, $location) {
         $scope.placeDetails = placeFactory.getPlace();
 
+        $scope.allOccurrences = $scope.placeDetails.occurrences.map(function(ocurrence) {
+          return { risk: ocurrence.risk, reports: ocurrence.reports };
+        });
+
         $scope.myGoBack = function() {
             $ionicHistory.goBack();
         };
