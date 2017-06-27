567e55a116da2de634e22205e0dd6a1ed3be9c22
Eduardo Braga <ebfjunior@gmail.com>
2017-06-26 21:36:35 -0300

    insere novo teste funcional para garantir que detalhes de ocorrências estão presentes na página PlaceDetails.

--- www/test/functional/page-objects/home.js
+++ www/test/functional/page-objects/home.js
@@ -10,6 +10,30 @@ var Home = function() {
     scrollIntoView(reportRiskButton);
     return reportRiskButton.click();
   };
+
+  this.fillSearchText = function(textToFill) {
+    var textField = element(by.model('search.text'));
+    textField.sendKeys(textToFill);
+    textField.sendKeys(protractor.Key.TAB);
+    return this;
+  };
+
+  this.clickSearchButton = function() {
+    var searchButton = element(by.buttonText('Buscar'));
+    browser.driver.sleep(10000);
+    searchButton.click();
+    return this;
+  };
+
+  this.clickRiskDetails = function() {
+    browser.wait(function() {
+      return element(by.repeater('place in places')).isPresent();
+    });
+
+    var lastPlace = element.all(by.repeater('place in places')).last();
+    lastPlace.click();
+    return this;
+  };
 };
 
 module.exports = Home;
