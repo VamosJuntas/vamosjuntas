dbc042264540ea9c0da834e1ba41fd6203b99dbf
Eduardo Braga <ebfjunior@gmail.com>
2017-06-28 14:28:49 -0300

    tentando configurar chrome headless

--- karma.conf.js
+++ karma.conf.js
@@ -78,7 +78,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome'],
 
     // customLaunchers for Travis CI
     customLaunchers: {
