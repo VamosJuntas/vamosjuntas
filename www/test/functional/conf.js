fdc2258258127ea55aca3d91cd26fd951784b73e
Eduardo Braga <ebfjunior@gmail.com>
2017-06-28 23:15:59 -0300

    isHeadless

--- www/test/functional/conf.js
+++ www/test/functional/conf.js
@@ -30,11 +30,12 @@ exports.config = {
   capabilities: {
     browserName: "chrome",
     chromeOptions: {
-      args: [ "--headless", "--disable-gpu", "--remote-debugging-port=9222", "--dump-dom" ],
+      args: [ "--headless", "--disable-gpu", "--remote-debugging-port=9222", "--dump-dom", "--no-sandbox" ],
       prefs: {
         "profile.default_content_setting_values.geolocation": 1,
       }
-    }
+    },
+    isHeadless: true
   },
 
   directConnect : true,
