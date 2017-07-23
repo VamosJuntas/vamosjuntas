5474e47e0f0ee0ac7b974c8f1cb02145acb0eb15
Josi Gama <jgama@thoughtworks.com>
2017-07-23 15:57:46 -0300

    Alterando os espaços em report.address

--- www/js/controller/report.address.controller.js
+++ www/js/controller/report.address.controller.js
@@ -1,33 +1,32 @@
 angular.module('vamosJuntas')
     .controller('ReportAddressController', ['$scope', '$stateParams', '$location', '$ionicHistory', 'placeFactory', 'reportFactory',
         function($scope, $stateParams, $location, $ionicHistory, placeFactory, reportFactory) {
+          $scope.report = {};
+          $scope.search = {};
+          $scope.search.text = placeFactory.getPlace() === undefined ? $stateParams.address : placeFactory.getPlace().address;
+          $scope.search.latitude = $stateParams.latitude;
+          $scope.search.longitude = $stateParams.longitude;
 
-            $scope.report = {};
-            $scope.search = {};
-            $scope.search.text = placeFactory.getPlace() === undefined ? $stateParams.address : placeFactory.getPlace().address;
-            $scope.search.latitude = $stateParams.latitude;
-            $scope.search.longitude = $stateParams.longitude;
+          $scope.myGoBack = function() {
+            $ionicHistory.goBack();
+          };
 
-            $scope.myGoBack = function() {
-                $ionicHistory.goBack();
-            };
+          var reportANewRisk = function() {
+            $scope.report.address = $scope.search.text;
+            $scope.report.latitude = $scope.search.latitude;
+            $scope.report.longitude = $scope.search.longitude;
 
-            var reportANewRisk = function() {
-                $scope.report.address = $scope.search.text;
-                $scope.report.latitude = $scope.search.latitude;
-                $scope.report.longitude = $scope.search.longitude;
+            reportFactory.createReport($scope.report).success(function(data, status, headers, config) {
+              $location.path('/confirmation');
+            }).error(function(data, status, header, config) {
+              $scope.hasError = true;
+            });
+          };
 
-                reportFactory.createReport($scope.report).success(function(data, status, headers, config) {
-                    $location.path('/confirmation');
-                }).error(function(data, status, header, config) {
-                    $scope.hasError = true;
-                });
-            };
-
-            $scope.submit = function(isFormValid) {
-                if (isFormValid) {
-                    reportANewRisk();
-                }
-            };
-        }
-    ]);
+          $scope.submit = function(isFormValid) {
+            if (isFormValid) {
+              reportANewRisk();
+            }
+          };
+        },
+      ]);
