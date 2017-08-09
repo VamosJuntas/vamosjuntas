45997fb983ff061bf68173105117fd9c641730cb
Eduardo Braga <ebfjunior@gmail.com>
2017-08-08 22:54:50 -0300

    adiciona logo na barra de navegação, troca estilo de botões

--- www/test/unit/controller/placeDetails.controller.test.js
+++ www/test/unit/controller/placeDetails.controller.test.js
@@ -20,23 +20,52 @@ describe('placeDetailsController', function() {
       };
     });
 
-    spyOn(placeFactory, 'getPlace').and.returnValue({title: 'Parada da João Pessoa', occurrences: []});
+    var occurrences = [
+      {
+        risk: 'Roubo',
+        numberOfOccurrences: 2,
+        reports: [{}, {}],
+      },
+      {
+        risk: 'Local Mal Iluminado',
+        numberOfOccurrences: 2,
+        reports: [{}, {}],
+      },
+    ];
+
+    var places = [
+      {title: 'Parada da João Pessoa, 1234', occurrences: occurrences},
+      {title: 'Avenida Ipiranga, 6681', occurrences: occurrences},
+    ];
+
+    spyOn(placeFactory, 'getPlace').and.returnValue(places);
   });
 
-  it('gets the risk places', function() {
+  fit('gets the places from the placesFactory', function() {
     createController();
     expect(placeFactory.getPlace).toHaveBeenCalled();
   });
 
   it('should get a place', function() {
     createController();
-    expect(scope.placeDetails.title).toBe('Parada da João Pessoa');
+    expect(scope.placeDetails.lenght).toBe(3);
   });
 
   it('should redirect to report page', function() {
     var placeController = createController();
     scope.submit();
     expect($location.path).toHaveBeenCalledWith('/report');
+  });
+
+  /*
+  $scope.allOccurrences = $scope.placeDetails.occurrences.map(function(ocurrence) {
+    return { risk: ocurrence.risk, reports: ocurrence.reports };
+  });
+  */
+
+  xit('gets all the occurrences in an array when the controller is created', function() {
+    var placeController = createController();
+    expect($scope.allOccurrences).toEqual([]);
   });
 
 });
